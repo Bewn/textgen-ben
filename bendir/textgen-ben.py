1b0140c0ff90200551a1ebd65d7fa6066d7494dd
Ben Rosebery <benrosebery@gmail.com>
2023-05-07 11:13:53 -0400

    housekeeping

---
+++ bendir/textgen-ben.py
@@ -0,0 +1,5 @@
+#this is the python front end for working with the provided text server and website etc
+# this is just my personal code, no warranty or expectation of function or permenance.
+# license: creative commons
+
+#TODO
