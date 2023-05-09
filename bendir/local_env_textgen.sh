af6f202e0b981788ee70a7414975f1438427634d
Ben Rosebery <benrosebery@gmail.com>
2023-05-09 00:55:40 -0400

    env fix, run source local_env_textgen.sh to get mamba env

--- bendir/local_env_textgen.sh
+++ bendir/local_env_textgen.sh
@@ -2,12 +2,12 @@
 #
 
 _SELF=$HOME/textgen-portable
-MAMBA_EXE=$_SELF/micromamba/bin/micromamba
+export MAMBA_EXE=$_SELF/micromamba/bin/micromamba
 
 hook_mamba () {
 	eval "$($MAMBA_EXE shell hook --shell=bash)"
 }
 
+export hook_mamba
 hook_mamba
-
 micromamba activate $_SELF/env_textgen
