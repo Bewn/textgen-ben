35e7f7ce5b281dc557f325a2523ea8ce2d83aa7d
Ben Rosebery <benrosebery@gmail.com>
2023-05-09 02:22:09 -0400

    added startup script base

--- bendir/startup.sh
+++ bendir/startup.sh
@@ -1,6 +1,30 @@
-#!/usr/bin/env bash
-#
-#
+#!/usr/bin/env sh
+# this code is based on the arch PKGBUILD in the build directory, but should be 
+# accessible to generic x86_64 distros
 #
 
-python textgen-ben.py
+_cwd=$PWD
+cd $_cwd
+prepare () {
+	cd $_cwd
+	if [ ! -d "$_cwd/textgen-portable" ]; then mkdir $_cwd/textgen-portable; fi
+	cd $_cwd/textgen-portable
+	_cwd=$_cwd/textgen-portable
+
+	#download latest micromamba
+	if [ ! -d $_cwd/micromamba ]; then mkdir $_cwd/micromamba && cd $_cwd/micromamba
+		wget https://micro.mamba.pm/api/micromamba/linux-64/latest && tar xf latest;
+	fi
+	export MAMBA_EXE=$_cwd/micromamba/bin/micromamba #micromamba now runnable from this script
+	
+	#make  environment
+	$MAMBA_EXE create --prefix=$_cwd/env_textgen
+	export _mamba_env=$_cwd/env_textgen
+}
+
+main () {
+    #pythonic bash with functional flair
+    $(prepare)
+}
+
+main
