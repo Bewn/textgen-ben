a4f3ccbce6c601773e4c5060f4cb7f1effe6a45e
Ben Rosebery <benrosebery@gmail.com>
2023-05-09 00:43:00 -0400

    fix hook

--- bendir/local_env_textgen.sh
+++ bendir/local_env_textgen.sh
@@ -3,4 +3,11 @@
 
 _SELF=$HOME/textgen-portable
 MAMBA_EXE=$_SELF/micromamba/bin/micromamba
-$MAMBA_EXE activate $_SELF/env_textgen
+
+hook_mamba () {
+	eval "$($MAMBA_EXE shell hook --shell=bash)"
+}
+
+hook_mamba
+
+micromamba activate $_SELF/env_textgen
